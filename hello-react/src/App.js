83c8527d12c4ae88cc941c54264a57022a25a4e6
mlicp1994 <mlicp1994@gmail.com>
2021-02-23 16:57:20 +0900

    React Study

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,37 @@
 import React, { Fragment } from 'react';
+import './App.css';
 
-function App(){
-    return(
-        <Fragment>
-            <h1>hi</h1>
-            <h2>hello</h2>
-        </Fragment>
-    )
-}
+
+
+// //2.4.5
+// function App() {
+  
+//   const name = undefined;
+
+//   // //내부에서 직접 렌더링은 가능
+//   // return <div>{name}</div>;
+
+//   //return name || '값이 undefinded입니다.'
+
+//   // //undefined를 return하면 오류발생
+//   // return name;
+// }
+
+
+
+// //2.4.4
+// function App(){
+//   const name = '뤼액트';
+//   return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+// }
+
+// function App(){
+//     return(
+//         <Fragment>
+//             <h1>hi</h1>
+//             <h2>hello</h2>
+//         </Fragment>
+//     )
+// }
 
 export default App;
