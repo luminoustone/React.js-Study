4f620b40c51a272c08d9fc59dc15019094a616ef
mlicp1994 <mlicp1994@gmail.com>
2021-03-04 17:27:55 +0900

    React.js Study : Todo App Complete

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e=>{
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            //submit이벤트는 브러우저에서 새로고침을 발생시킨다.
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요."/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요."
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd/>
             </button>
