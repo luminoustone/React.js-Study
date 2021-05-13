94dcab180c40c0f78b6438d9ef8bd71a9440854c
mlicp1994 <mlicp1994@gmail.com>
2021-05-13 15:41:07 +0900

    Implement item insert Function in App.js TodoInsert.js

--- todo-app2/src/App.js
+++ todo-app2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,9 +22,24 @@ const App = ()=> {
     },
   ])
 
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current +=1;
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
+      <TodoInsert onInsert={onInsert}/>
       <TodoList todos={todos}/>
     </TodoTemplate>
   );
--- todo-app2/src/components/TodoInsert.js
+++ todo-app2/src/components/TodoInsert.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] =useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');
+        e.preventDefault();
+        },[onInsert, value]);
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd/>
             </button>
@@ -13,6 +29,5 @@ const TodoInsert = () => {
     );
 
 };
-
 
 export default TodoInsert;
