53adcac3e17ce5bbaed5b1e59b327e6a2c9bf0b3
mlicp1994 <mlicp1994@gmail.com>
2021-05-13 12:13:17 +0900

    Added Todoinsert.js, Todinsert.scss

--- todo-app2/src/App.js
+++ todo-app2/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 
+
 const App = ()=> {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert/>
+    </TodoTemplate>
+  );
 };
 
 export default App;
