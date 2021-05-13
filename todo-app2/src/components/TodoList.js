cc2afa9a2641aed2649a14d55ffdf7e785b91099
mlicp1994 <mlicp1994@gmail.com>
2021-05-13 14:12:59 +0900

    Implement the function

--- todo-app2/src/components/TodoList.js
+++ todo-app2/src/components/TodoList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
+import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
     return (
         <div className="TodoList">
-            <TodoListItem/>
-            <TodoListItem/>
-            <TodoListItem/>
+             {todos.map(todo => (
+                 <TodoListItem todo={todo} key={todo.id}/>
+             ))} 
         </div>
     );
 };
