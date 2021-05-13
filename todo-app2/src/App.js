9d82b91a53b5c0a01f5b6c14fc443c9fad66f6cb
mlicp1994 <mlicp1994@gmail.com>
2021-05-13 16:21:46 +0900

    Chapter11 : Set starting ch.11

--- todo-app2/src/App.js
+++ todo-app2/src/App.js
@@ -3,24 +3,20 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = ()=> {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id:3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i<=2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ])
+    });
+  }
+  return array;
+}
+
+const App = ()=> {
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4);
 
