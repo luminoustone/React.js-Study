ca1c48c884f56267acfe83564bea6217ddd9eec1
mlicp1994 <mlicp1994@gmail.com>
2021-05-18 14:45:09 +0900

    Setting Router-tutorial project

---
+++ router-tutorial/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>이 프로젝트는 리엑트 라우터 기초를 실슬해 보는 에제 프로젝트이다......</p>
+        </div>
+    );
+};
+
+export default About;
