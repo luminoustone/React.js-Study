c0a7ebbff3e0dc313268bee3122a53977e3212cf
mlicp1994 <mlicp1994@gmail.com>
2021-05-20 10:37:08 +0900

    Added NewsList.js NewsItem.js

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async ()=> {
-    try{
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=b615e45da4b44b1391185f124a2f6722',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} /> }
-    </div>
-  );
+    <>
+      <Categories/>
+      <NewsList/>
+    </>
+  )
 };
 
 export default App;
---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import NewsItem from './NewsItem';
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (amx-width: 768px) {
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`;
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        //async를 사용하는 함수를 따로 선언(useEffect는 직접적으로 async를 사용불가능)
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=b615e45da4b44b1391185f124a2f6722',
+                );
+                setArticles(response.data.articles);
+            } catch(e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    //대기 중일 때
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>;
+    }
+    //아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+        return null;
+    }
+
+    //articles 값이 유효할 때
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
+        </NewsListBlock>
+    );
+};
+
+export default NewsList;
