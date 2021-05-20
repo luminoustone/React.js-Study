852e00b7a9cace97160f5c67184c10fca9b0632c
mlicp1994 <mlicp1994@gmail.com>
2021-05-20 14:18:33 +0900

    Use Context.provider in App.js

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
 const App = () => {
   return (
-    <div>
-      <ColorBox/>
-    </div>
+    <ColorContext.Provider value={{color: 'red'}}>
+      <div>
+        <ColorBox/>
+      </div>
+    </ColorContext.Provider>
   );
 };
 
