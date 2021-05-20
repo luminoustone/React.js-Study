45cb51f69f7f0b65f7652fac326e579072dfedd2
mlicp1994 <mlicp1994@gmail.com>
2021-05-20 14:11:20 +0900

    Setting Context-tutorial
    -added color.js in contexts
    -added ColorBox.js in components

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorBox from './components/ColorBox';
+
+const App = () => {
+  return (
+    <div>
+      <ColorBox/>
+    </div>
+  );
+};
+
+export default App;
