29c8ee98ad2b4802643de50e54c6140e8808ef11
mlicp1994 <mlicp1994@gmail.com>
2021-05-20 09:30:05 +0900

    Added NewsAPI in app.js

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import axios from '../node_modules/axios/index';
+import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async ()=> {
+    try{
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=b615e45da4b44b1391185f124a2f6722',
+      );
       setData(response.data);
-    })
-  }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <div>
       <div>
