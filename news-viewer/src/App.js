3e23b57908e25eb6e3a982dbed3cab73d5cce463
mlicp1994 <mlicp1994@gmail.com>
2021-05-20 10:59:04 +0900

    Added Categories.js
    -added style in category

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import axios from 'axios';
 import NewsList from './components/NewsList';
 import Categories from './components/Categories';
 
 const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+
   return (
     <>
-      <Categories/>
-      <NewsList/>
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category}/>
     </>
   )
 };
