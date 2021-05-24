a36eb9fc766100f9c75ba3ee3812a06d60460ec7
mlicp1994 <mlicp1994@gmail.com>
2021-05-24 14:50:34 +0900

    Refactor RestApi

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -14,9 +14,19 @@ const SampleContainer = ({
 }) => {
     //클래스 형태 컴포넌트였다면 componenetDidMount
     useEffect(() => {
-        getPost(1);
-        getUsers(1);
+        // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+        // 그 내부에서 async 함수를 선언하고 호출해 줍니다.
+        const fn = async () => {
+            try {
+                await getPost(1);
+                await getUsers(1);
+            } catch (e) {
+                console.log(e)
+            }
+        };
+        fn();
     }, [getPost, getUsers]);
+
     return (
         <Sample
             post={post}
@@ -28,11 +38,11 @@ const SampleContainer = ({
 };
 
 export default connect(
-    ({ sample }) => ({
+    ({ sample, loading }) => ({
         post: sample.post,
         users: sample.users,
-        loadingPost: sample.loading.GET_POST,
-        loadingUsers: sample.loading.GET_USERS
+        loadingPost: loading['sample/GET_POST'],
+        loadingUsers: loading['sample/GET_USERS']
     }),
     {
         getPost,
--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,111 +1,36 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
-// 액션 타입을 선언합니다.
-// 한 요청당 세 개를 만들어야 합니다.
+import createRequestThunk from '../lib/createResquestThunk';
 
+// 액션 타입을 선언합니다.
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 //thunk 함수를 생성합니다.
-//thunk 함수 내부에서는 시작할 때, 성공했을 때, 시류ㅐ했을 때 다른 액션을 디스패치한다
+//thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치한다
 
-export const getPost = id => async dispatch => {
-    dispatch({ type: GET_POST });// 요청을 시작한 것을 알림
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        }); // 요청 성공
-    } catch (e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-        }); // 에러 발생
-        throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해줌
-    }
-};
-
-export const getUsers = () => async dispatch => {
-    dispatch({ type: GET_USERS }); 
-    try {
-        const response = await api.getUsers();
-        dispatch({
-            type: GET_USERS_SUCCESS,
-            payload: response.data
-        }); //성공
-    } catch (e) {
-        dispatch({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-        });
-        throw e;
-    }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기 상태를 선언합니다.
 // 요청의 로딩 중 상태는 loddiang이라는 객체에서 관리합니다.
-
 const initialState = {
-    loading: {
-        GET_POST: false,
-        GET_USERS: false
-    },
     post: null,
     users: null
 };
+
 const sample = handleActions(
     {
-        [GET_POST]: state => ({
+        [GET_POST_SUCCESS]: (state, action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: true // 요청 시작
-            }
-        }),
-        [GET_POST_SUCCESS]: (state,action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false // 요청 완료
-            },
             post: action.payload
-        }),
-        [GET_POST_FAILURE]: (state,action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false // 요청 완료
-            }
-        }),
-        [GET_USERS]: state => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: true // 요청 시작
-            }
         }),
         [GET_USERS_SUCCESS]: (state,action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false // 요청 완료
-            },
             users: action.payload
-        }),
-        [GET_USERS_FAILURE]: (state,action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false // 요청 완료
-            }
         })
     },
     initialState
