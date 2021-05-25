5f8105245681273724003000154baf3811ca1e65
mlicp1994 <mlicp1994@gmail.com>
2021-05-25 10:57:16 +0900

    Import Redux in project
    -import redux, react-redux, redux-actions, immer,
    redux-devtools-extension
    -added auth.js(reducer function)
    -adde index.js(root reducer) in modules
    -applied redux to project using Provider at src/index.js

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules/index';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>
+ ,
   document.getElementById('root')
 );
 
