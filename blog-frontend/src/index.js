44e6e57d5f7522e762923cfaa2264b0bf6dff34b
mlicp1994 <mlicp1994@gmail.com>
2021-05-25 17:20:01 +0900

    Added API integration logic
    -added porxy code using proxy server in package.json
    -added axios instance create logic in lib/api/client.js
    -added loading.js reducer and register that at root reducer
    -added redux-saga using middleware

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './modules/index';
+import rootReducer, { rootSaga } from './modules/index';
+import { createSagaMiddleware } from 'redux-saga';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,8 +1,15 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { createRequestActionTypes } from '../lib/createRequestSaga';
+import createRequestSaga from '../../../learn-redux-middleware/src/lib/createRequestSaga';
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes('auth/REGISTER',);
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes('auth/LOGIN',);
 
 export const changeField = createAction(
     CHANGE_FIELD,
@@ -13,6 +20,22 @@ export const changeField = createAction(
     }),
 );
 export const initializeForm = createAction(INITIALIZE_FORM, form => form);  // register, login
+export const register = createAction(REGISTER, ({ username, password }) => ({
+    username,
+    password,
+}));
+export const login = createAction(LOGIN, ({ username, password}) => ({
+    username,
+    password,
+}))
+
+//create saga
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+    yield takeLatest(REGISTER, registerSaga);
+    yield takeLatest(LOGIN, loginSaga);
+}
 
 const initialState = {
     register: {
@@ -24,6 +47,8 @@ const initialState = {
         username: '',
         password: '',
     },
+    auth: null,
+    authError: null,
 };
 
 const auth = handleActions(
@@ -35,6 +60,29 @@ const auth = handleActions(
         [INITIALIZE_FORM]: (state, { payload: form}) => ({
             ...state,
             [form]: initialState[form],
+            authError: null, // when converting form, Initialize authentication error
+        }),
+        //register success
+        [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        //register failure
+        [REGISTER_FAILURE]: (state, { payload: error }) => ({
+            ...state,
+            authError: error,
+        }),
+        //login success
+        [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        //login failure
+        [LOGIN_FAILURE]: (state, { payload: error }) => ({
+            ...state,
+            authError: error,
         }),
     },
     initialState,
